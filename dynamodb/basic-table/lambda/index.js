e67fb273b00c37eebfa0587a5360f6ba9a220d77
Allen McPherson <al@losalamosal.me>
2022-09-28 00:33:31 +0000

    Creating DynamoDB table; no IAM yet

--- dynamodb/basic-table/lambda/index.js
+++ dynamodb/basic-table/lambda/index.js
@@ -1,4 +1,13 @@
 // TODO: CommonJS or ES6 module???? Explore.
 exports.handler = async (event, context) => {
-  console.log("Getting started");
+  //console.log(JSON.stringify(event));
+  //console.log(JSON.stringify(context));
+  //console.log(JSON.stringify(process.env));
+  let ret = {
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: process.env.DB_TABLE_NAME
+  };
+  return ret;
 };
