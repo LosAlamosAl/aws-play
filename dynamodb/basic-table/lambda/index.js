44c5d7f0399a0ebb8f7aabc9b03d256a95a4abe9
Allen McPherson <al@losalamosal.me>
2022-09-28 01:13:56 +0000

    DynamoDB lambda access failing (IAM)

--- dynamodb/basic-table/lambda/index.js
+++ dynamodb/basic-table/lambda/index.js
@@ -1,4 +1,8 @@
 // TODO: CommonJS or ES6 module???? Explore.
+
+const { DynamoDBClient, DescribeTableCommand } = require("@aws-sdk/client-dynamodb");
+const dynoClient = new DynamoDBClient();
+
 exports.handler = async (event, context) => {
   //console.log(JSON.stringify(event));
   //console.log(JSON.stringify(context));
@@ -9,5 +13,19 @@ exports.handler = async (event, context) => {
     headers: { "Access-Control-Allow-Origin": "*" },
     body: process.env.DB_TABLE_NAME
   };
-  return ret;
+
+  const params = {
+    TableName: process.env.DB_TABLE_NAME
+  };
+
+  const descTableCommand = new DescribeTableCommand(params);
+
+  try {
+    const data = await dynoClient.send(descTableCommand);
+    ret.body = JSON.stringify(data);
+    return ret;
+  } catch (error) {
+    console.log(`descTableCommand failed: ${JSON.stringify(error)}`);
+    throw new Error(JSON.stringify(error));
+  }
 };
